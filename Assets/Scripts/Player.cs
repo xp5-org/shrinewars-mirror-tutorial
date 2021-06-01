f00dfec3e391f4d9d5c689e31bdb41f2781d5aaf
xp5-org <kylematthewholst@gmail.com>
2021-05-31 18:42:34 -0700

    a working demo here, now to add on more stuff
    GetAxis("Vert / Horiz") takes joystick input. check build settings, cannot excluse josystick input. its incorrectly reading input from a driver i believe, related to the 3d space mouse. unployyed joystick and also disabled joystick software with no change.
    
    workaround is to check for W / A / S / D input on keyboard and ignore the horizontal / veritcal input
    
    Saving this here. everyone can control their own cube and seems to support 4 players.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    void HandleMovement()
+    {
+        if (isLocalPlayer)
+        {
+            //3d space mouse driver is causing some problem with unity
+            //Horizontal
+            float moveHorizontal = 0;
+            if (Input.GetKey(KeyCode.A))
+                moveHorizontal = -1;
+            if (Input.GetKey(KeyCode.D))
+                moveHorizontal = 1;
+
+            //Vertical
+            float moveVertical = 0;
+            if (Input.GetKey(KeyCode.S))
+                moveVertical = -1;
+            if (Input.GetKey(KeyCode.W))
+                moveVertical = 1;
+
+
+            Vector3 movement = new Vector3(moveHorizontal * 0.01f, moveVertical * 0.01f, 0);
+            transform.position = transform.position + movement;
+
+        }
+    }
+    
+    void Update()
+    {
+        HandleMovement();
+        print("Input: " + Input.GetAxis("Horizontal"));
+        print("Input: " + Input.GetAxis("Vertical"));
+
+    }
+}
